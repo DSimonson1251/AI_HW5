393b7f6c346412da72953897de435ce9fd4ec526
Daniel Simonson <danielsimonson@email.arizona.edu>
2024-07-30 18:12:53 -0700

    updated for hw

--- working.py
+++ working.py
@@ -7,6 +7,7 @@ ls = os.listdir(data_dir)
 print(ls)
 
 sample = pd.read_csv('histopathologic-cancer-detection/sample_submission.csv')
+print(sample.shape)
 print(sample.head())
 
 train = pd.read_csv('histopathologic-cancer-detection/train_labels.csv', dtype=str)
@@ -15,3 +16,21 @@ print(train.head())
 
 # train.id = train.id + '.tif'
 # train.head()
+import matplotlib.pyplot as plt
+import cv2
+
+# Load the labels
+labels = pd.read_csv('histopathologic-cancer-detection/train_labels.csv')
+print("labels: ", labels.head())
+
+def show_images(images, labels, num=10):
+    for i in range(num):
+        plt.subplot(2, 5, i+1)
+        img_path = os.path.join('histopathologic-cancer-detection/train', images[i] + '.tif')
+        img = cv2.imread(img_path)
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title('Label: {}'.format(labels[i]))
+        plt.axis('off')
+    plt.show()
+
+show_images(labels['id'].values,labels['label'].values)
